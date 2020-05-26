130b2af50042f97ecd90ecbeccdd9a4dacb39878
“Alex <alexmorris10x@icloud.comgit config --global user.name alexmorris10x>
2020-05-26 18:26:23 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+function App(): any {
+  return (
+    <>
+      <TitleWrapper>My Store front!</TitleWrapper>
+      <ItemWrapper>
+        <div>
+          <div>Item 1</div>
+          <div>My cool item</div>
+          <BuyButton1Wrapper>Press</BuyButton1Wrapper>
+        </div>
+        <div>
+          <div>Item 2</div>
+          <div>My cool item</div>
+          <BuyButton2Wrapper>Press</BuyButton2Wrapper>
+        </div>
+      </ItemWrapper>
+    </>
+  );
+}
+
+export default App;
+
+const TitleWrapper = styled.div`
+  text-align: center;
+  font-size: 5em;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const BuyButton1Wrapper = styled.button`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const BuyButton2Wrapper = styled.button`
+  display: flex;
+  justify-content: space-evenly;
+`;
